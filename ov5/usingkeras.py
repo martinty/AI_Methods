ec19b8704c65ec7f1700fdebd2b279b0f16135c6
Martin Tysseland <martinty@stud.ntnu.no>
2020-04-04 11:24:15 +0200

    Update new model

--- ov5/usingkeras.py
+++ ov5/usingkeras.py
@@ -1,16 +1,15 @@
 import pickle
 import tensorflow as tf
-from keras.models import Sequential
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
-from keras.layers import Embedding, LSTM, Dense
-from keras.optimizers import RMSprop
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.optimizers import RMSprop
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import plot_confusion_matrix
 import numpy as np
 
 import scikitplot as skplt
-
 
 
 data = pickle.load(open("keras-data.pickle", "rb"))
@@ -23,43 +22,41 @@ vocab_size = data["vocab_size"]
 max_length = data["max_length"]
 
 # Preprocess data
-x_train = pad_sequences(x_train, maxlen=max_length)
-x_test = pad_sequences(x_test, maxlen=max_length)
+x_train = pad_sequences(x_train, maxlen=512)
+x_test = pad_sequences(x_test, maxlen=512)
 y_train = to_categorical(y_train, num_classes=2)
 y_test = to_categorical(y_test, num_classes=2)
 
 
-
- # Init the model
+# Init the model
 model = Sequential()
 
 # Add layers as specified in the assignment
-model.add(Embedding(input_dim = vocab_size, output_dim=2))
-model.add(LSTM(units=2, activation = 'relu'))
+model.add(Embedding(input_dim=vocab_size, output_dim=2))
+model.add(LSTM(units=2))
 model.add(Dense(units=2))
 
 
 print("Compile time")
- # Compile and train
-model.compile(loss='categorical_crossentropy', optimizer=RMSprop(), metrics=['accuracy'])
+# Compile and train
+model.compile(loss='mean_squared_error', optimizer='Adam', metrics=['accuracy'])
 
 print("Model fitting")
 
-history = model.fit(x_train, y_train, epochs=3, batch_size=64)
+history = model.fit(x_train, y_train, epochs=3, batch_size=128, verbose=1)
 
-title= "Normalized confusion matrix for the model"
+title = "Normalized confusion matrix for the model"
 
 print("Model fitted")
-y_predicted = model.predict_classes(x=x_test, batch_size=64)
-skplt.metrics.plot_confusion_matrix(y_test, y_predicted,normalize=True, title=title)
+y_predicted = model.predict_classes(x=x_test, batch_size=128)
+skplt.metrics.plot_confusion_matrix(y_test, y_predicted, normalize=True, title=title)
 
-#print(title)
-#print(disp.confusion_matrix)
+# print(title)
+# print(disp.confusion_matrix)
 plt.show()
 
 
-
-#THE FOLLOWING CODE IS COPIED FROM THE KERAS.IO WEBPAGE: https://keras.io/visualization/
+# THE FOLLOWING CODE IS COPIED FROM THE KERAS.IO WEBPAGE: https://keras.io/visualization/
 
 # Plot training & validation accuracy values
 plt.plot(history.history['acc'])
